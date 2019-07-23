2e6cf33b3cc5e4e383fe63d31eff903a53cbf67c
thousand7274 <thousand7274@gmail.com>
2019-07-23 15:08:46 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+// 上から順番に処理される
+// 80点以上　合格
+// 50点以　惜しい
+// 30点以上　残念
+// 30点以下　また来年
+// 変数（命名を考える）(代入可能)
+// console.logで出力
+// 開発者ツールを使う
+
+
+let score = 90;
+
+if(score >= 80) {
+console.log(`${score}`+'点！合格')
+} else if(score >= 50) {
+  console.log(`${score}`+'点！惜しい');
+} else if(score >= 30) {
+  console.log(`${score}`+'点,残念');
+} else if(score <= 30){
+  console.log(`${score}`+'点,また来年');
+}
+
+if(score >= 80) {
+  console.log(`${score}点！合格`);
+  } else if(score >= 50) {
+    console.log(`${score}点！惜しい`);
+  } else if(score >= 30) {
+    console.log(`${score}点 残念`);
+  } else {
+    console.log(`${score}点 また来年`);
+  }
